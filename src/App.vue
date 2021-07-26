9a44468125e29d0de27df037a8cd973e2fb153b0
Halim Tekin <thehalimtekin@gmail.com>
2021-07-26 22:50:12 +0300

    update 🧙🏻‍♂️

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Vue uygulamasına hoşgeldiniz gençler!" />
+    <HelloWorld msg="Vue uygulamasına hoşgeldiniz!" />
   </div>
 </template>
 
